95a02aaa367a4e4d4287982efa8c54a7bb8b6a53
Noam <rozenblat09@gmail.com>
2022-04-26 12:55:32 +0300

    first commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cors = require('cors')
 const app = express();
 
 
-app.use(cors({credentials: true, origin: 'http://localhost:3002'}))
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
